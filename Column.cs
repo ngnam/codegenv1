58ed86c1038659935f5c15b83a812314c393b346
ngnam <nguyenvannam0411@gmail.com>
2024-10-27 21:17:00 +0700

    update

---
+++ Column.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeGeneratorV1
+{
+    public class Column
+    {
+        public int Id { get; set; }
+        public string DataField { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        public string DataType { get; set; }
+        public int MaxLength { get; set; }
+        public int? Position { get; set; }
+        public Column(int id, string dataField, bool isPrimaryKey, string dataType, int maxLength, int? position)
+        {
+            Id = id;
+            DataField = dataField;
+            IsPrimaryKey = isPrimaryKey;
+            DataType = dataType;
+            MaxLength = maxLength;
+            Position = position;
+        }
+    }
+
+}
---
+++ Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGeneratorV1
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Column> Columns { get; set; }
+        public Table()
+        {
+            Columns = new HashSet<Column>();
+        }
+    }
+}
